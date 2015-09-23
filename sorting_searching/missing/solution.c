03fe6367a6af771ca9b86d71278cae3e4425e186
Filipe Goncalves <filipe@codinghighway.com>
2015-09-24 00:00:20 +0100

    Updated "missing" description

--- sorting_searching/missing/solution.c
+++ sorting_searching/missing/solution.c
@@ -1,3 +1,28 @@
+/* You are given an array of N unique integers such that each integer i
+ * satisfies 0 <= i < 2n.
+ *
+ * Write a program that prints every integer x, where 0 <= x < 2n, such that x
+ * does not occur in the array.
+ *
+ * Strive for O(n) time complexity and O(1) space complexity.
+ *
+ * EXAMPLE
+ *
+ * Input:
+ *
+ * [ 0, 2, 4 ]
+ *
+ * Output:
+ * [ 1, 3, 5 ]
+ *
+ * Explanation:
+ *
+ * We have n = 3, so numbers are in the range [0..5].
+ * The numbers in the array are [ 0, 2, 4 ], so the missing numbers in the range
+ * are [ 1, 3, 5 ].
+ *
+ * Source: Careercup (Google interview)
+ */
 
 #include <stdio.h>
 
